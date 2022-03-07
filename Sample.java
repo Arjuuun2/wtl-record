2dee9ee57e9f405a373b82b54c0e62c47aee41b6
Arjuuun2 <arjunvijay233@gmail.com>
2022-03-07 08:20:17 +0530

    first commit

---
+++ Sample.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Sample{
+ public static void main(String ar[])
+{
+System.out.println("hello world");
+} 
+}
---
+++ php4.php
@@ -0,0 +1,5 @@
+<?php 
+echo "The date and time is :";
+$myDate = date("d-m-y h:i:s");
+echo $myDate; 
+?>
